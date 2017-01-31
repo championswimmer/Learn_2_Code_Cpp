9ded6a0eee3c8c66e05bc25c0d7e40fb2c27485d
Arnav Gupta <championswimmer@gmail.com>
2017-02-01 03:09:25 +0530

    operators
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ 01basics/04operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << b / a << endl;
+    cout << a % b << endl;
+
+    cout << a++ << endl;
+    cout << ++b << endl;
+
+    cout << a << endl;
+    cout << b << endl;
+}
