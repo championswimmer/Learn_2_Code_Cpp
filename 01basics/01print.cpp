b7aca903aa15d8ce2f2914fdb7251918bb026845
Arnav Gupta <championswimmer@gmail.com>
2017-02-01 03:05:23 +0530

    intro
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ 01basics/01print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+
+    cout << "a" << endl << "b";
+
+    cout << 10;
+
+    cout << "new line \n new line";
+}
---
+++ 01basics/03types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    bool b = true;
+    char c = 'c';
+    double d = 12.34;
+
+    cout << a << endl << b  << endl << c << endl << d;
+}
